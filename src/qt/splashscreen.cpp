17d2659b57405631897da8843bafeb531995289a
danrachita <38796007+danrachita@users.noreply.github.com>
2019-01-26 13:49:35 +0200

    Update splashscreen.cpp

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -35,9 +35,9 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) 
 
     // define text to place
     QString titleText = tr("Xando");
-    QString versionText = tr("v1.0.0.1");
+    QString versionText = tr("v2.0.0.0");
     QString copyrightTextBtc = QChar(0xA9) + QString(" 2009-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Bitcoin Core developers"));
-    QString copyrightTextXDO = QChar(0xA9) + QString(" 2018 ").arg(COPYRIGHT_YEAR) + QString(tr("The Xando developers"));
+    QString copyrightTextXDO = QChar(0xA9) + QString(" 2019 ").arg(COPYRIGHT_YEAR) + QString(tr("The Xando Then P2P developers"));
     QString titleAddText = networkStyle->getTitleAddText();
     QString font = QApplication::font().toString();
 
